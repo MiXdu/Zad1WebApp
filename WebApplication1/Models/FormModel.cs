db96b47c4ce94a6dea48268d63bdc51e433864d7
mikolaj.duszyk <mikolaj.duszyk@MIKOLAJ>
2022-01-14 20:16:50 +0100

    Dodaj pliki projektów.

---
+++ WebApplication1/Models/FormModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class FormModel
+    {
+     [Display(Name = "Imię")]
+     public string Name { get; set; }
+     [Display(Name = "Data Urodzenia")]
+     public DateTime DataUrodzenia { get; set; }
+    }
+}
